81d1dd062766b180ade91106e99950f6dc913dfe
Shohboz <vip.shohboz@gmail.com>
2018-02-14 00:02:31 +0300

    Remove status from create issue form

--- src/routes/Issue/components/Form.js
+++ src/routes/Issue/components/Form.js
@@ -27,7 +27,6 @@ class Form extends Component {
       handleSubmit,
       valid,
       form,
-      optionsStatus,
       optionsUsers,
       optionsProjects,
       optionsDepartments,
@@ -60,13 +59,6 @@ class Form extends Component {
             date={this.state.due_date}
             onChange={this.onChangeDueDate}
             component={RenderPicker}
-          />
-          <Field
-            name="status"
-            label="Статус"
-            component={RenderSelect}
-            optionsToRender={optionsStatus}
-            validate={[required]}
           />
           <Field
             name="assignee_id"
